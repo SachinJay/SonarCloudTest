3b6950ee944d47f3335695eb1d3ec7c7a90aebb5
SachinJay <sjd232@cornell.edu>
2022-10-30 18:33:07 -0400

    Added dummy endpoints

---
+++ src/main/java/com/example/demo/services/DummyService.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyService {
+
+    public String getMsg() {
+        return "Up and at 'em!";
+    }
+}
